f6543de4f90eee8d30386e82a3daab9373c7d814
mahdi <m.mahdi.kht@gmail.com>
2024-07-07 00:00:59 +0330

    Create model and migration for messages

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function messages() {
+        return $this->hasMany(Message::class);
+    }
 }
